b3dd2ef414749369f5f562fcd3c125b88234c942
aamargant <arnau.ad@gmail.com>
2025-09-01 00:51:53 +0200

    refactor: remove unused Image import and fallback image from Posts component

--- app/components/pages/Posts.tsx
+++ app/components/pages/Posts.tsx
@@ -1,4 +1,3 @@
-import Image from "next/legacy/image";
 import Link from "next/link";
 import { postsQuery } from "@/lib/sanity.query";
 import { PostType } from "@/types";
@@ -10,8 +9,6 @@ import { sanityFetch } from "@/lib/sanity.client";
 import { readTime } from "@/app/utils/readTime";
 import { toPlainText } from "@portabletext/react";
 
-const fallbackImage: string =
-  "https://res.cloudinary.com/arnau/image/upload/v1692608339/arnau/blog.png";
 
 export default async function Posts() {
   const posts: PostType[] = await sanityFetch({
@@ -28,19 +25,9 @@ export default async function Posts() {
               <article key={post._id}>
                 <Link
                   href={`/blog/${post.slug}`}
-                  className="flex lg:flex-row flex-col lg:items-center items-start gap-8 dark:bg-primary-bg bg-secondary-bg p-6 rounded-lg border dark:border-zinc-800 border-zinc-200 group"
+                  className="flex lg:flex-row flex-col lg:items-center items-start dark:bg-primary-bg bg-secondary-bg p-6 rounded-lg border dark:border-zinc-800 border-zinc-200 group"
                 >
-                  <div className="relative lg:w-[450px] lg:h-52 w-full h-56 overflow-clip">
-                    <Image
-                      src={post.coverImage?.image || fallbackImage}
-                      className="dark:bg-zinc-800 bg-zinc-100 rounded-md object-cover group-hover:scale-125 duration-300"
-                      alt={post.coverImage?.alt || post.title}
-                      layout="fill"
-                      placeholder={post.coverImage ? "blur" : "empty"}
-                      blurDataURL={post.coverImage?.lqip || ""}
-                    />
-                  </div>
-                  <div className="max-w-lg">
+                  <div className="max-w-full">
                     <h2 className="max-w-sm text-2xl font-semibold tracking-tight mb-4">
                       {post.title}
                     </h2>
